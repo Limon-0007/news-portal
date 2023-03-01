5a11bd30167c4f7035cc5c4c1bec1b7fd76bcb1b
Limon-0007 <limonahmed3023@gmail.com>
2023-03-02 00:29:15 +0600

    added site resources

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const data = async() => {
+  const url = 'https://openapi.programming-hero.com/api/news/categories'
+  const res = await fetch(url);
+  const data = await res.json();
+  getData(data.data);
+}
+
+const getData = data => {
+    console.log(data);
+    // data.forEach(categories => {
+    //     console.log(categories);
+    // })
+}
+
+data()
