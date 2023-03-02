429f071a1f0177131cdb2c6d7281c3a7811a65cc
Limon-0007 <limonahmed3023@gmail.com>
2023-03-02 14:09:02 +0600

    added all categories section

--- js/script.js
+++ js/script.js
@@ -9,9 +9,8 @@ const getData = data => {
     //  console.log(data);
     const categoriesSection = document.getElementById('categories')
     data.forEach(categories => {
-        //console.log(categories);
         const div = document.createElement('div');
-        div.innerHTML += `<p><a class="text-gray-600 font-semibold text-sm" href="#">${categories.category_name}</a></p>`
+        div.innerHTML += `<p><a onclick="getCards('${categories.category_id}', '${categories.category_name}')" class="text-gray-600 font-semibold text-sm" href="#">${categories.category_name}</a></p>`
         categoriesSection.appendChild(div)
     })
 }
@@ -20,24 +19,27 @@ data()
 
 // get the cards
 
-const getCards = async(category_id) => {
+const getCards = async(category_id, category_name) => {
     const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
     const res = await fetch(url);
     const data = await res.json();
-    getCardsData(data.data);
+    getCardsData(data.data, category_name);
 }
 
-getCards('03')
-
-const getCardsData = data => {
-    // console.log(data);
+const getCardsData = (data, category_name) => {
+    // show categories
+    const newsCount = document.getElementById('news-count');
+    newsCount.innerText = `${data.length}`;
+    const newsCategory = document.getElementById('news-category');
+    newsCategory.innerText = `${category_name}`
+    // cards
     const cards = document.getElementById('cards');
+    cards.innerHTML = "";
     data.forEach(items => {
-        console.log(items);
         const singleCard = document.createElement('div');
         singleCard.className = 'mb-5'
         singleCard.innerHTML += `<div class="card card-side bg-base-100 shadow-xl">
-        <img class="w-1/4" src="${items.image_url}" alt="Movie"/>
+        <img class="md:w-2/6" src="${items.image_url}" alt="Movie"/>
         <div class="card-body">
           <h2 class="card-title text-lg font-lg">${items.title}</h2>
           <p class="font-lg text-md">${items.details.substring(200, 'cut')}</p>
@@ -46,8 +48,8 @@ const getCardsData = data => {
           <div class="flex gap-4">
           <img class="w-10 rounded-full" src="${items.author.img}" alt="">
           <div>
-              <p class="text-sm font-semibold">${items.author.name}</p>
-              <p class="text-sm font-semibold">${items.author.published_date}</p>
+              <p class="text-sm font-semibold">${items.author.name ? items.author.name : "No data found"}</p>
+              <p class="text-sm font-semibold">${items.author.published_date ? items.author.published_date : "No data found"}</p>
           </div>
       </div>
 
@@ -56,7 +58,7 @@ const getCardsData = data => {
           <p>${items.total_view}</p>
       </div>
 
-      <div>
+      <div class="text-orange-600">
       <i class="fa-solid fa-star"></i>
       <i class="fa-solid fa-star"></i>
       <i class="fa-solid fa-star"></i>
@@ -65,7 +67,7 @@ const getCardsData = data => {
       </div>
 
       <div>
-      <i class="fa-solid fa-arrow-right"></i>
+      <i class="fa-solid fa-right-long text-2xl text-blue-500"></i>
       </div>
           </div>
         </div>
@@ -73,5 +75,3 @@ const getCardsData = data => {
       cards.appendChild(singleCard)
     })
 }
-
-// getCardsData('01')
